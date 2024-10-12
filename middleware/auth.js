e11e48f2178d8ccf29ec655e977d99889980a99a
Ayushi Gupta <ayushi.gupta.038@gmail.com>
2024-10-13 01:53:55 +0530

    Add middleware for authentication

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,1 +1,17 @@
 //Verify JWT token
+import jwt from 'jsonwebtoken';
+
+const auth = (req, res, next) => {
+    const token = req.header('Authorization')?.replace('Bearer ', '');
+    if(!token) return res.status(401).send('Access denied. No token provided.');
+
+    try {
+        const decoded = jwt.verify(token, 'secretKey');
+        req.user = decoded;
+        next();
+    } catch(err) {
+        res.status(400).send('Invalid token.');
+    }
+};
+
+export default auth;
